5650f8cbbf11f62cc464a449905a25b82ffcd0b4
2017tmxkxm <2017tmxkxm@gmail.com>
2025-03-11 12:37:08 +0900

    [Mod] 독립 실행형 서블릿 애플리케이션으로 전환

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,9 +1,51 @@
 package tobyspring.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless Standalone Application");
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// Front Controller
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						// request
+						String name = req.getParameter("name");
+
+						// HelloController를 활용한 Front Controller
+						String ret = helloController.hello(name);
+
+						// response
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if (req.getRequestURI().equals("/user")) {
+
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+
+
+        });
+		webServer.start();
 	}
 
 }
